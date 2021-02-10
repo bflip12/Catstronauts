39f646f9ddaf778c9908f102a45ddfc4b0525e09
bflip12 <bflip12@gmail.com>
2021-02-09 20:58:51 -0500

    initial

---
+++ server/src/schema.js
@@ -0,0 +1,27 @@
+const { gql } = require("apollo-server");
+
+const typeDefinitions = gql`
+    type Query {
+        "Query to get tracks array for the homepage "
+        tracksForHome: [Track!]!
+    }
+
+    "A track is a group of modules that teaches about a specific topic"
+    type Track {
+        id: ID!
+        title: String!
+        author: Author!
+        thumbnail: String
+        length: Int
+        modulesCount: Int
+    }
+
+    "Author of a complete Track"
+    type Author {
+        id: ID!
+        name: String!
+        photo: String
+    }
+`;
+
+module.exports = typeDefinitions;
